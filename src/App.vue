02c5b484c43d014203433fd1bb176212e478702f
Daniel <danielfealmeida@gmail.com>
2026-07-24 23:31:32 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import BibleColumn from "./components/bible-column.vue";
+import ara from "./data/ARA.json"
+
+let book = 0;
+let chapter = 0;
+
+</script>
+
+<template>
+  <div class="bg-dark">
+    <div class="navx w-100 bg-dark border-bottom border-secondary mb-2 pb-1">
+      <div class="mx-auto h-100 p-1 w-25 d-flex justify-content-center">
+        <select class="s1 me-2 rounded-pill border-secondary ps-2 bg-dark text-white" name="books" id="books" v-model="book" v-on:change="$forceUpdate(); chapter=0;">
+          <option v-for="(book, index) in ara" :key="index" :value="index">{{ book.name }}</option>
+        </select>
+        <select class="me-2 rounded-pill border-secondary ps-2 bg-dark text-white" name="chapters" id="chapters" v-model="chapter" v-on:change="$forceUpdate();">
+          <option v-for="(chapter, index) in ara[book].chapters" :key="index" :value="index">{{ index+1 }}</option>
+        </select>
+      </div>
+    </div>
+
+    <div class="d-sm-inline-flex">
+      <BibleColumn :book="book" :chapter="chapter" defaultVersion="nvi"/>
+      <BibleColumn class="d-none d-md-block" :book="book" :chapter="chapter" defaultVersion="naa"/>
+      <BibleColumn class="d-none d-md-block" :book="book" :chapter="chapter" defaultVersion="ara"/>
+      <BibleColumn class="d-none d-md-block" :book="book" :chapter="chapter" defaultVersion="arc"/>
+    </div>
+    
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/bible-column.vue
@@ -0,0 +1,52 @@
+<script setup>
+
+import nvi from "../data/NVI.json"
+import naa from "../data/NAA.json"
+import ara from "../data/ARA.json"
+import arc from "../data/ARC.json"
+
+const props = defineProps(["book", "chapter", "defaultVersion"]);
+
+let version = props.defaultVersion;
+
+</script>
+
+<template>
+  <div class="ch text-light w-sm-25 mr-auto px-3 bg-dark overflow-scroll border-end border-secondary">
+    <div class="mx-auto my-2 w-25 d-flex justify-content-center px-2">
+        <select class="me-2 rounded-pill ps-2 bg-dark text-white border-1 border-secondary" name="version" id="version" v-model="version" v-on:change="$forceUpdate();">
+            <option key="ara" value="ara">ARA</option>
+            <option key="arc" value="arc">ARC</option>
+            <option key="nvi" value="nvi">NVI</option>
+            <option key="naa" value="naa">NAA</option>
+        </select>
+    </div>
+
+    <div class="mb-2" v-if="version=='nvi'" v-for="(verse, index) in nvi[props.book].chapters[props.chapter]" :key="index">
+        <b class="text-primary fw-normal">{{ index+1 }}.</b>
+        {{ verse }}
+    </div>
+    <div class="mb-2" v-if="version=='ara'" v-for="(verse, index) in ara[props.book].chapters[props.chapter]" :key="index">
+        <b class="text-primary fw-normal">{{ index+1 }}.</b>
+        {{ verse }}
+    </div>
+    <div class="mb-2" v-if="version=='naa'" v-for="(verse, index) in naa[props.book].chapters[props.chapter]" :key="index">
+        <b class="text-primary fw-normal">{{ index+1 }}.</b>
+        {{ verse }}
+    </div>
+    <div class="mb-2" v-if="version=='arc'" v-for="(verse, index) in arc[props.book].chapters[props.chapter]" :key="index">
+        <b class="text-primary fw-normal">{{ index+1 }}.</b>
+        {{ verse }}
+    </div>
+  </div>
+</template>
+
+<style>
+    .ch {
+        height: 93.5vh;
+        min-width: 25%;
+        overflow-x: hidden !important;
+        scrollbar-color: var(--scroll-dark) var(--bg-dark) ;
+        scrollbar-width: thin;
+    }
+</style>
